bd91d6dbb0dd929b791bac810e815e44beaaa20d
Vixies1337 <tiengiang02975@gmail.dom>
2024-10-25 12:45:40 +0700

    add few list pages

---
+++ src/app/(dashboard)/list/parents/page.tsx
@@ -0,0 +1,83 @@
+import Link from 'next/link';
+import Pagination from "@/components/Pagination";
+import TableSearch from "@/components/TableSearch";
+import Table from "@/components/Table";
+import Image from 'next/image';
+import { role, parentsData } from "@/lib/data";
+
+
+type Parent = {
+    id: number;
+    name: string;
+    students: string[];
+    phone: string;
+    email?: string;
+    address: string;
+};
+
+const columns = [
+    { header: "Thông tin", accessor: "info" },
+    { header: "Tên sinh viên", accessor: "students", className: "hidden md:table-cell" },
+    { header: "SĐT", accessor: "phone", className: "hidden lg:table-cell" },
+    { header: "Địa chỉ", accessor: "address", className: "hidden lg:table-cell" },
+    { header: "Hành động", accessor: "actions" },
+];
+
+const ParentListPage = () => {
+    const renderRow = (item: Parent) => (
+        <tr key={item.id} className='border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-PYellowLight'>
+            <td className='flex items-center gap-4 p-4 '>
+                <div className="flex flex-col">
+                    <h3 className="font-semibold">{item.name}</h3>
+                    <p className="text-xs text-gray-500">{item?.email}</p>
+                </div>
+            </td>
+            <td className="hidden md:table-cell">{item.students.join(",")}</td>
+            <td className="hidden md:table-cell">{item.phone}</td>
+            <td className="hidden md:table-cell">{item.address}</td>
+            <td>
+                <div className="flex items-center gap-2">
+                    <Link href={`/list/teacher/${item.id}`} legacyBehavior>
+                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-PCyan">
+                            <Image src="/view.png" alt="" width={16} height={16} />
+                        </button>
+                    </Link>
+                    {role === "admin" && (
+                        <button className="w-7 h-7 flex items-center justify-center rounded-full bg-PPurple">
+                            <Image src="/delete.png" alt="" width={16} height={16} />
+                        </button>
+                    )}
+                </div>
+            </td>
+        </tr>
+    );
+
+    return (
+        <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+            {/* TOP */}
+            <div className="flex items-center justify-between">
+                <h1 className="hidden md:block text-lg font-semibold">Phụ Huynh</h1>
+                <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+                    <TableSearch />
+                    <div className="flex items-center gap-4 self-end">
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-PCyan">
+                            <Image src="/filter.png" alt="" width={20} height={20} />
+                        </button>
+                        <button className="w-8 h-8 flex items-center justify-center rounded-full bg-PCyan">
+                            <Image src="/sort.png" alt="" width={20} height={20} />
+                        </button>
+                        {role === "admin" && (<button className="w-8 h-8 flex items-center justify-center rounded-full bg-PCyan">
+                            <Image src="/plus.png" alt="" width={20} height={20} />
+                        </button>)}
+                    </div>
+                </div>
+            </div>
+            {/* LIST */}
+            <Table columns={columns} renderRow={renderRow} data={parentsData} />
+            {/* PAGINATION */}
+            <Pagination />
+        </div>
+    );
+};
+
+export default ParentListPage;
