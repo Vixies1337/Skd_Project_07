776e3e2be720b8b47d18ab11ea2a822981de1a01
Vixies1337 <tiengiang02975@gmail.dom>
2024-11-04 10:32:16 +0700

    Finished FormModel

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{}"
+self.__REACT_LOADABLE_MANIFEST="{\"components\\\\FormModel.tsx -> ./forms/StudentForm\":{\"id\":\"components\\\\FormModel.tsx -> ./forms/StudentForm\",\"files\":[\"static/chunks/_app-pages-browser_src_components_forms_StudentForm_tsx.js\"]},\"components\\\\FormModel.tsx -> ./forms/TeacherForm\":{\"id\":\"components\\\\FormModel.tsx -> ./forms/TeacherForm\",\"files\":[\"static/chunks/_app-pages-browser_src_components_forms_TeacherForm_tsx.js\"]}}"
